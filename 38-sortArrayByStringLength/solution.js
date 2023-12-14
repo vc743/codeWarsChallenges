53a2de849d8fccdf31ce2a5b49e01b8d02867717
vc743 <victorcontrerasvargas55@gmail.com>
2023-12-14 11:27:33 -0400

    Challenge #38

---
+++ 38-sortArrayByStringLength/solution.js
@@ -0,0 +1,5 @@
+function sortByLength (array) {
+    // Return an array containing the same strings, ordered from shortest to longest
+    return array.sort((a, b) => a.length > b.length ? 1 : -1);
+};
+  

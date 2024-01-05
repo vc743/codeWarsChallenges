3e01447e46745cd4af952cc177f4272c3997f05b
vc743 <victorcontrerasvargas55@gmail.com>
2024-01-05 15:33:26 -0400

    Challenge #59

---
+++ 59-Welcome!/solution.js
@@ -0,0 +1,28 @@
+function greet(language) {
+    let DB = {
+        "english": "Welcome",
+        "czech": "Vitejte",
+        "danish": "Velkomst",
+        "dutch": "Welkom",
+        "estonian": "Tere tulemast",
+        "finnish": "Tervetuloa",
+        "flemish": "Welgekomen",
+        "french": "Bienvenue",
+        "german": "Willkommen",
+        "irish": "Failte",
+        "italian": "Benvenuto",
+        "latvian": "Gaidits",
+        "lithuanian": "Laukiamas",
+        "polish": "Witamy",
+        "spanish": "Bienvenido",
+        "swedish": "Valkommen",
+        "welsh": "Croeso"
+  }
+    if ((language === "IP_ADDRESS_INVALID") || 
+    (language === "IP_ADDRESS_NOT_FOUND") || 
+    (language === "IP_ADDRESS_REQUIRED")) {
+      return DB["english"];
+    }else{
+      return DB[language];
+    }
+}
